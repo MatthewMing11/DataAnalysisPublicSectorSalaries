99a7adbddc372bed2c4ea998969aac6a46757403
MatthewMing11 <Matthew.Ming11@myhunter.cuny.edu>
2021-12-01 11:00:28 -0500

    quick deploy

---
+++ project.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df=pd.read_csv("Citywide_Payroll_Data__Fiscal_Year_.csv")
+print(df)
+fiscal=df[["Fiscal Year","Regular Gross Paid"]]
+print(fiscal)
+fiscal=fiscal.groupby("Fiscal Year").mean()
+print(fiscal)
+plt.plot(fiscal.index,fiscal['Regular Gross Paid'])
+plt.show()
+table=pd.pivot_table(df,index=['Fiscal Year','Title Description'],values="Regular Gross Paid", aggfunc=np.mean)
+print(table)
