35432d2718595255b5d5080708b1d4688bd4828c
MatthewMing11 <Matthew.Ming11@myhunter.cuny.edu>
2021-12-10 02:05:44 -0500

    please push to main

--- project.py
+++ project.py
@@ -1,14 +1,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 df=pd.read_csv("Citywide_Payroll_Data__Fiscal_Year_.csv")
-print(df)
+df["Work Location Borough"]=df["Work Location Borough"].str.capitalize()
 #table 1
 fiscal=df[["Fiscal Year","Regular Gross Paid"]]
-print(fiscal)
 fiscal=fiscal.groupby("Fiscal Year").mean()
-print(fiscal)
 plt.style.use('fivethirtyeight')
 plt.figure(figsize=(13,8))
 plt.xlabel("Year")
@@ -16,19 +15,32 @@ plt.ylabel("Regular Gross Paid($)")
 plt.plot(fiscal.index,fiscal['Regular Gross Paid'])
 plt.savefig("static/graph.png",bbox_inches="tight")
 plt.show()
-table=pd.pivot_table(df,index=['Fiscal Year','Title Description'],values="Regular Gross Paid", aggfunc=np.mean)
-print(table)
 #table 2
 plt.style.use('fivethirtyeight')
-plt.figure(figsize=(400,8))
-plt.xlabel("County")
+plt.figure(figsize=(13,8))
+plt.xlabel("Borough")
 plt.ylabel("Regular Gross Paid($)")
 plt.tick_params(axis="x",labelsize=14)
-borough=df[["Work Location Borough","Regular Gross Paid"]]
-borough["Work Location Borough"]=borough["Work Location Borough"].str.lower()
-borough=borough.groupby("Work Location Borough").mean()
-plt.bar(borough.index,borough['Regular Gross Paid'],align="edge")#,width=0.3)
-plt.savefig("static/graph2.png",bbox_inches="tight")
+table=pd.pivot_table(df,index=['Work Location Borough','Fiscal Year'], values="Regular Gross Paid",aggfunc=np.mean)
+table=table.loc[["Bronx","Brooklyn","Queens","Manhattan","Richmond"]]
+splot=sns.barplot(x="Work Location Borough",y="Regular Gross Paid",hue="Fiscal Year",data=table.reset_index())
+#this helps move the legend so graph is very readable
+plt.legend(bbox_to_anchor=(1, 1), loc=2, borderaxespad=0)
+#following functions help correctly display and save the graph
+sfig=splot.get_figure()
+sfig.savefig("static/graph2.png",bbox_inches="tight")
 plt.show()
 #table 3
 plt.style.use('fivethirtyeight')
+plt.figure(figsize=(13,8))
+plt.xlabel("Borough")
+plt.ylabel("Regular Gross Paid($)")
+plt.tick_params(axis="x",labelsize=14)
+borough=df[["Work Location Borough","Regular Gross Paid"]]
+borough=borough.groupby("Work Location Borough").mean()
+borough=borough.loc[["Bronx","Brooklyn","Queens","Manhattan","Richmond"]]
+borough=borough.rename(index={"Richmond":"Staten Island"})
+plt.bar(borough.index,borough['Regular Gross Paid'])
+plt.savefig("static/graph3.png",bbox_inches="tight")
+plt.show()
+
